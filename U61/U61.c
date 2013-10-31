0b2ba7230eac8a3419325db815bed70df613f311
Manfred <manfred@linuxmint.linuxmint-domain>
2013-10-31 16:15:09 +0100

    Fixade U61 :)

--- U61/U61.c
+++ U61/U61.c
@@ -1,24 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int fib(int n){
-  int a = 0;
-  int b = 1;
-  for(int i = 1; i <= n; i++){
-    int fib_i = a + b;
-    a = b;
-    b = fib_i;
+int fib(int n) {
+  int *a = malloc(sizeof(int));
+  int *b = malloc(sizeof(int));
+  *a = 0;
+  *b = 1;
+  for (int i=1; i<=n; ++i) {
+    int fib_i = *a + *b; 
+    *a = *b; 
+    *b = fib_i;
   }
-  return a;
+  free(b);
+  return a; 
 }
 
-int main(int argc, char **argv){
-  if(argc < 2){
+int main(int argc, char **argv) {
+  if (argc < 2) {
     puts("Usage: ./fib-iter 5");
-  }
-  else{
+  } else {
     int n = atoi(*(argv+1));
-    printf("fib(%d) = %d\n", n, fib(n));
+    int* fibon = fib(n);
+    printf("fib(%d) = %d\n", n, *fibon);
+    free(fibon);
   }
   return 0;
 }
